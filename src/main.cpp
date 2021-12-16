413dc84b788f3b238f44657be702d6d6cc2ebde1
Masoud Yousefi <masoudyousefi.1967@gmail.com>
2021-12-16 19:01:16 +0330

    add vector

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
+#include <vector>
 #include "electrician.hpp"
 #include "wire.hpp"
 #include "lamp.hpp"
+#include "tool.hpp"
 using namespace std;
 
 int main()
 {
-	tool *a = new wire(12.2, 1, 2, 20.5);
-	tool *b = new lamp(1, 2, 15);
+	vector<tool *> tools; // tools vector
 
-	a->print_info();
-	b->print_info();
+	try
+	{
+		tools.push_back(new wire(12.2, 1, 2, 20.5));
+	}
+	catch (const exception &e)
+	{
+		cerr << e.what() << endl;
+	}
+	try
+	{
+		tools.push_back(new lamp(1, 2, 15));
+	}
+	catch (const exception &e)
+	{
+		cerr << e.what() << endl;
+	}
+	tools[0]->print_info();
+	tools[1]->print_info();
 
-	cout << (*a) + (*b) << endl;
-	cout << *a + 20 << endl;
-	cout << *b - 5 << endl;
+	cout << (*tools[0]) + (*tools[1]) << endl;
+	cout << *tools[0] + 20 << endl;
+	cout << *tools[1] - 5 << endl;
 
 	return 0;
-} //end of phase #2
+} // end of phase #2
